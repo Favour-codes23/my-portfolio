7956ff2aa8c4fb94831e832721ec3ff04daa8fa9
Favour Nwokochaw <favour7nwokocha@gmail.com>
2025-09-10 18:08:33 +0100

    fix vite build output directory to dist

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
   build: {
-    outDir: 'project/build', // 👈 This ensures Vite outputs here
-  },
+    outDir: 'dist'
+  }
 })
